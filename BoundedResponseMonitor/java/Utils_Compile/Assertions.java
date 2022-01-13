5a1c1e462f01ea1ea7c220f3ae32a55b050d19a0
Eric Mercer <egm@cs.byu.edu>
2022-01-13 10:37:09 -0700

    Update README and BonudedResponse with more examples

--- BoundedResponseMonitor/java/Utils_Compile/Assertions.java
+++ BoundedResponseMonitor/java/Utils_Compile/Assertions.java
@@ -7,14 +7,31 @@ public class Assertions {
     org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
   }
 
+  static public void expectEquals(TypeDescriptor<BigInteger> T, BigInteger expected, BigInteger actual) {
+    org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+  }
+
   static public void assertEquals(TypeDescriptor<Boolean> T, Boolean expected, Boolean actual) {
     org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
   }
+
+  static public void expectEquals(TypeDescriptor<Boolean> T, Boolean expected, Boolean actual) {
+    org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+  }
+
   static public void assertTrue(TypeDescriptor<Boolean> T, Boolean condition) {
     org.junit.jupiter.api.Assertions.assertTrue(condition);
   }
 
+  static public void expectTrue(TypeDescriptor<Boolean> T, Boolean condition) {
+    org.junit.jupiter.api.Assertions.assertTrue(condition);
+  }
+
   static public void assertFalse(TypeDescriptor<Boolean> T, Boolean condition) {
+    org.junit.jupiter.api.Assertions.assertFalse(condition);
+  }
+
+  static public void expectFalse(TypeDescriptor<Boolean> T, Boolean condition) {
     org.junit.jupiter.api.Assertions.assertFalse(condition);
   }
 }
